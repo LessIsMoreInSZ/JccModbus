08e3272c65469a03ba30e103b160a261977e43b3
jyj <2534803898@qq.com>
2024-07-29 14:15:09 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # JccModbus
 A modbus pooled register communication library
+
+一个modbus的注册通信库，目前第一版本居于HSLcommunication。
+
+本库支持商用，如需商用无需联系酱醋茶，但是hslcommunication库版权归原作者所有，如需商业使用，请联系原作者胡工。
--- TestJccModbus/Program.cs
+++ TestJccModbus/Program.cs
@@ -20,15 +20,33 @@ namespace TestJccModbus
             modbusTcpJober.Connect();
             //modbusTcpJober.Register(def);
 
-            JccVariableDef def = new JccVariableDef();
-            def.ModbusDataType = ModbusDataType.Coils;
-            def.DataType = JccDataType.Bool;
-            def.VarId = "0";
+            //JccVariableDef def = new JccVariableDef();
+            //def.ModbusDataType = ModbusDataType.Coils;
+            //def.DataType = JccDataType.Bool;
+            //def.VarId = "0";
+
+            JccVariableDef def = ReturnDef("0");
+            JccVariableDef def1 = ReturnDef("1");
+            JccVariableDef def2 = ReturnDef("2");
+            JccVariableDef def3 = ReturnDef("3");
+
 
 
             JccEventDispathcher jccEventDispathcher = new JccEventDispathcher(modbusTcpJober);
             jccEventDispathcher.RegisterVar(def, OnDataChange);
+            jccEventDispathcher.RegisterVar(def1, OnDataChange);
+            jccEventDispathcher.RegisterVar(def2, OnDataChange);
+            jccEventDispathcher.RegisterVar(def3, OnDataChange);
 
+        }
+
+        static JccVariableDef ReturnDef(string address)
+        {
+            JccVariableDef def = new JccVariableDef();
+            def.ModbusDataType = ModbusDataType.Coils;
+            def.DataType = JccDataType.Bool;
+            def.VarId = address;
+            return def;
         }
 
         private static void OnDataChange(JccVariableDef def, JccVariableValue value)
